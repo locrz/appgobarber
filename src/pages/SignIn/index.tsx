375888ae44a93330782e30613510d7e53a4cc1d0
Locrz <locrz@MBP-de-Danilo-Pela.local>
2020-10-01 08:13:03 -0300

    Create signin screen and add external fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Image, Text} from 'react-native';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.png';
 
-const SignIn: React.FC = () => <Container />
+import {Container, Title} from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logo} />
+    <Title>Faça seu logon</Title>
+  </Container>
+);
 
 export default SignIn;
